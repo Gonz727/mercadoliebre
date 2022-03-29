a160f9942c37a7934566f5231318721837106db3
Gonz727 <fernandez.gonzalo727@gmail.com>
2022-03-28 22:47:53 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app= express();
+
+const publicPath = path.resolve(__dirname,'./public/')
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3030, () => {console.log("Servidor Andando") 
+})
+
+
+
+app.get("/",(req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/h.html"))})
+
+
+app.get("/login",(req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))}) 
+       
+ app.get("/register",(req, res) => {
+     res.sendFile(path.resolve(__dirname, "./views/register.html"))})
